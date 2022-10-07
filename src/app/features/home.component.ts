4bd8f03f14e65689cfd6557d7575693336d4cfb9
Fabio Biondi <hello@fabiobiondi.io>
2022-10-07 11:43:05 +0200

    add logo

--- src/app/features/home.component.ts
+++ src/app/features/home.component.ts
@@ -3,7 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   template: `
-    <h1>RxJS 7 Master class</h1>
+    <div style="display: flex; align-items: center">
+      <img src="assets/murena.png" width="100">
+      <h1>RxJS 7 Master class</h1>
+    </div>
   `,
 })
 export class HomeComponent implements OnInit {
