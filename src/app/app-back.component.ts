c0a216931cb5c475a6d3bc00e52541cb0d98b7e7
Fabio Biondi <hello@fabiobiondi.io>
2022-10-07 23:16:07 +0200

    added for the presentation

---
+++ src/app/app-back.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <app-navbar></app-navbar>
+    <router-outlet></router-outlet>
+  `,
+})
+export class AppComponent {
+
+}
---
+++ src/app/core/services/timer.service.ts
@@ -0,0 +1,9 @@
+// timer.service.ts
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TimerService {
+
+}
